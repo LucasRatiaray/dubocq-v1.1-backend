b1d4afa5de293c22c0a48cae9d9f8a54a8c84744
Lucas Ratiaray <dev@lucasratiaray.fr>
2025-01-27 14:52:41 +0100

    Add Zone model, migration and seeder

---
+++ database/seeders/ZoneSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Zone;
+
+class ZoneSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $zones = [
+            ['zone_name' => 'Zone 1b', 'min_km' => 0, 'max_km' => 0, 'rate' => 1.75],
+            ['zone_name' => 'Zone 2', 'min_km' => 1, 'max_km' => 10, 'rate' => 2.5],
+            ['zone_name' => 'Zone 3', 'min_km' => 11, 'max_km' => 20, 'rate' => 3.75],
+            ['zone_name' => 'Zone 4', 'min_km' => 21, 'max_km' => 30, 'rate' => 4.5],
+            ['zone_name' => 'Zone 4', 'min_km' => 31, 'max_km' => 40, 'rate' => 5.5],
+            ['zone_name' => 'Zone 4', 'min_km' => 50, 'max_km' => null, 'rate' => 8.3],
+        ];
+
+        foreach($zones as $zone) {
+            Zone::create($zone);
+        }
+    }
+}
