1c1a6c5bc45020fc284f143c63810e555cd1a725
Lucas <lucas@informatique-m2i.fr>
2025-02-03 16:54:36 +0100

    remove back api controller and routes

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -7,8 +7,6 @@ use App\Models\Employee;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->apiResource('employees', EmployeeController::class);
-
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
