da22a1f640e3a75ee2681b8e93d3076e922d9418
Lucas <lucas@informatique-m2i.fr>
2025-02-03 17:33:03 +0100

    add resource for employees

---
+++ app/Http/Resources/V1/EmployeeResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources\V1;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class EmployeeResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'type' => 'employee',
+            'id' => $this->id,
+            'attributes' => [
+                'type' => $this->type,
+                'status' => $this->status,
+                'created_at' => $this->created_at,
+                'updated_at' => $this->updated_at,
+            ],
+            'links' => [
+               ['self' => route('employees.show', ['employee' => $this->id]),]
+            ],
+        ];
+    }
+}
